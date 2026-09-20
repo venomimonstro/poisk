77b326b06a7a2c12ccb9b6ceec0a1d8ebda5696f
venomimonstro <lisencko.art@gmail.com>
2026-09-20 16:16:25 +0300

    Sprint 15: persist orphan address import state

---
+++ db/migrations/000013_address_orphan_state.sql
@@ -0,0 +1,10 @@
+ALTER TABLE address_staging_rows
+    DROP CONSTRAINT IF EXISTS address_staging_rows_state_check;
+
+ALTER TABLE address_staging_rows
+    ADD CONSTRAINT address_staging_rows_state_check
+    CHECK (state IN ('VALID','APPLIED','ORPHAN'));
+
+CREATE INDEX idx_address_staging_orphans
+    ON address_staging_rows(batch_id,region_code,object_id)
+    WHERE state='ORPHAN';
