0b45eeb5ad00ee7d43e77ed1be9bb114a839ef75
venomimonstro <lisencko.art@gmail.com>
2026-09-20 19:19:26 +0300

    sprint17: wire public widget search runtime

---
+++ cmd/app/widget_routes.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/venomimonstro/poisk/internal/growth/widget"
+	widgethttp "github.com/venomimonstro/poisk/internal/growth/widget/httpapi"
+	"github.com/venomimonstro/poisk/internal/platform/config"
+	"github.com/venomimonstro/poisk/internal/platform/guard"
+	searchbackend "github.com/venomimonstro/poisk/internal/search/backend"
+)
+
+func registerWidgetRoutes(router chi.Router,apiGuard guard.Middleware,cfg config.Config,pool *pgxpool.Pool)error{
+	backend,err:=searchbackend.New(searchbackend.Config{BaseURL:fmt.Sprintf("http://%s:%d",cfg.ManticoreHost,cfg.ManticoreHTTPPort),MaxResults:40});if err!=nil{return err}
+	repo:=widget.NewRepository(pool)
+	handler:=widgethttp.Handler{
+		Widgets:repo,
+		Search:widget.Service{Backend:backend,Usage:repo},
+		KeyLimiter:guard.NewLimiter(50,100,10000,10*time.Minute),
+		ClientLimiter:guard.NewLimiter(5,10,50000,10*time.Minute),
+	}
+	router.Handle("/api/widget/search",apiGuard.Protect(handler))
+	return nil
+}
