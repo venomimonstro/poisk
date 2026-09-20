e4a5a2ee9ee793d4a2d04d79c0ec9e86192d4a27
venomimonstro <lisencko.art@gmail.com>
2026-09-20 10:58:39 +0300

    sprint 11: add blocked-site error

--- internal/webmaster/errors.go
+++ internal/webmaster/errors.go
@@ -2,4 +2,7 @@ package webmaster
 
 import "errors"
 
-var ErrInvalidInput = errors.New("invalid webmaster input")
+var (
+	ErrInvalidInput = errors.New("invalid webmaster input")
+	ErrSiteBlocked  = errors.New("site is blocked by crawl policy")
+)
