107cc8f24aed194c4fc8537391e8fd52d3f9bd98
venomimonstro <lisencko.art@gmail.com>
2026-09-21 17:21:20 +0300

    sprint 17: wire agency routes

---
+++ cmd/app/agency_routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	agencyhttp "github.com/venomimonstro/poisk/internal/growth/agency/httpapi"
+	"github.com/venomimonstro/poisk/internal/growth/agency"
+	"github.com/venomimonstro/poisk/internal/platform/guard"
+	"github.com/venomimonstro/poisk/internal/webmaster"
+)
+
+func registerAgencyRoutes(router chi.Router,apiGuard guard.Middleware,pool *pgxpool.Pool,auth *webmaster.Service){
+	h:=agencyhttp.Handler{Auth:auth,Agencies:agency.NewRepository(pool)}
+	router.Mount("/api/agency",apiGuard.Protect(h.Routes()))
+}
